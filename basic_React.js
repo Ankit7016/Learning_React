4ae03a96dcc97fc9b7bae0b734d861d0c593e455
Ankit7016 <96678148+Ankit7016@users.noreply.github.com>
2024-07-24 09:52:25 +0530

    this is react install and use without CDN links

--- basic_React.js
+++ basic_React.js
@@ -1,8 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const newRoot = React.createElement("div", { id: "Div" }, [
-  React.createElement("H1", { id: "heading" }, "This iS H1 Tag"),
-  React.createElement("H2", { id: "heading" }, "This iS H2 Tag"),
-  React.createElement("H3", { id: "heading" }, "This iS H3 Tag"),
-  React.createElement("H4", { id: "heading" }, "This iS H4 Tag")
+  React.createElement("h1", { id: "heading." }, "This iS H1 Tag"),
+  React.createElement("h2", { id: "heading.." }, "This iS H2 Tag"),
+  React.createElement("h3", { id: "heading..-" }, "This iS H3 Tag"),
+  React.createElement("h4", { id: "heading..." }, "This iS H4 Tag"),
+  React.createElement("h4", { id: "heading...." }, "This iS H4 Tag")
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
